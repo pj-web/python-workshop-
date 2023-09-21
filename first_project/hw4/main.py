d64490123fb3437f1e6a23a12e1d6fcfa37ec8f2
PJ <69799563+pj-web@users.noreply.github.com>
2023-09-22 02:10:12 +0400

    20230921

--- first_project/hw4/main.py
+++ first_project/hw4/main.py
@@ -1,3 +1,48 @@
 from cities import cities
 
-print(cities)
+cities_set = set()
+for city_name in cities:
+    cities_set.add(city_name["name"])
+
+print(cities_set)
+print(len(cities_set))
+used_cities = cities_set.pop()
+# print(used_cities)
+
+# user_input = input(f'Мой ход первый: {used_cities}, Введите название города: ')
+# print(user_input)
+
+while cities_set:
+    user_input = input(f'Введите название города: ')
+    while user_input != 'стоп':
+        if user_input in cities_set:
+            print(len(cities_set))
+            last_char = user_input[-1]
+            # next_city = {city for city in cities_set if cities_set}
+            # used_cities = cities_set.pop()
+            for city in cities_set:
+                if city[0] == last_char.upper():
+                    # print(city)
+                    cities_set.discard(city)
+                    user_input = input(f'Последняя буква: {last_char}: {city}, ваш ход:  ')
+                    break
+        # elif user_input not in cities_set:
+        #     user_input = input(f'Город {user_input} не подходит! Введите другой: ')
+        else:
+            user_input = input(f'Город {user_input} не подходит (else)! Введите другой: ')
+            break
+
+        #         next_move = [city for city in cities_set if city[0] == last_char]
+        # user_input = input(f'Последняя буква {last_char}, Город: {next_city} ')
+        # for city in cities_set:
+        # if cities_set[0] == last_char:
+        #     user_input = input(f'Мой ход {city}')
+        # next_move = [city for city in cities_set if city[0] == last_char]
+
+    else:
+        print('Ха-ха-ха! Ты проиграл, человек!')
+        break
+
+
+
+
