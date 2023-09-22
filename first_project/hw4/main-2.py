cb8be1189c0f7b87922c4ae4ea474c93d5c21dca
PJ <69799563+pj-web@users.noreply.github.com>
2023-09-23 01:15:36 +0400

    upd20230922

--- first_project/hw4/main-2.py
+++ first_project/hw4/main-2.py
@@ -1,33 +1,39 @@
 from cities import cities
 
+# получаем названия городов из импортированного списка словарей
 cities_set = set()
 for city_name in cities:
-    cities_set.add(city_name["name"])
+    cities_set.add(city_name["name"].lower())
 
-print(cities_set)
-print(f'1: {len(cities_set)}')
+user_input = input('Ведите название города, \nбез Ь, Ы, Й на конце: ').lower()
 
-user_input = input('Пользовательский ввод: ')
-
+# проверяем полученное название первого города, от которого будем отталкиваться в основном цикле
 if user_input == 'стоп':
-    print('Вы сдались!')
-elif user_input[-1] == 'ь' or user_input[-1] == 'ы':
-    user_input = input('Введите другое название: ')
+    print('Мы еще не начали, а вы уже сдались!')
+    exit()
+# проверяем чтобы слова не заканчивались на 'ь' 'ы' 'й'
+elif user_input[-1] == 'ь' or user_input[-1] == 'ы' or user_input[-1] == 'й':
+    user_input = input('Введите другое название: ').lower()
 else:
-    if user_input in cities_set:
-        cities_set.remove(user_input)
-        print(f'2: {len(cities_set)}')
+    if user_input in cities_set:  # проверяем наличие введенного названия во множестве
+        cities_set.remove(user_input)  # удаляем введенное название из множества
+        print(f'Мой вариант на {user_input[-1].upper()}')
 
+# основной цикл игры
 for city in list(cities_set):
-    if city[0] == user_input[-1].upper() and city[-1] != 'ь' and city[-1] != 'ы':
-        cities_set.remove(city)
-        user_input = input(f'Город {city}, ваш вариант на {city[-1].upper()}: ')
-        if user_input in cities_set and user_input[0] == city[-1].upper():
+    # выбираем название города для хода компьютера
+    if city in cities_set and city[0] == user_input[-1] and city[-1] != 'ь' and city[-1] != 'ы' and city[-1] != 'й':
+        cities_set.remove(city)  # удаляем выбранное название из множества городов
+        # компьютер делает ход и предлагает пользователю сделать следующий ход
+        user_input = input(f'Город {city.capitalize()}, ваш вариант на {city[-1].upper()}: ').lower()
+        # проверяем следующий ход пользователя
+        if (user_input in cities_set and user_input[0] == city[-1]
+                and user_input[-1] != 'ь' and user_input[-1] != 'ы' and user_input[-1] != 'й'):
             cities_set.remove(user_input)
-            print(f'4: {len(cities_set)} правильно')
+            print(f'Хороший ход, теперь мой вариант на {user_input[-1].capitalize()}')
         elif user_input == 'стоп':
             print('Вы сдались!')
             break
         else:
-            print('Неправильно, вы проиграли!')
+            print(f'{user_input.capitalize()} не подходит! Вы проиграли!')
             break
--- first_project/hw4/main.py
+++ first_project/hw4/main.py
@@ -1,53 +1,35 @@
 from cities import cities
 
+# получаем названия городов из импортированного списка словарей
 cities_set = set()
 for city_name in cities:
     cities_set.add(city_name["name"])
-
 print(cities_set)
-print(f'1: {len(cities_set)}')
-# first_city = cities_set.pop()
-# print(first_city)
-# user_input = input(f'Мой первый ход: {first_city} \nВведите город на {first_city[-1].upper()}: ')
 
-# user_input = input(f'Мой ход первый: {used_cities}, Введите название города: ')
-# print(user_input)
+user_input = input('Ведите название города: ').capitalize()
 
-while cities_set:
-    # user_input = input(f'Введите название города: ')
-    while user_input != 'стоп':
-        if user_input in cities_set:
-            cities_set.remove(user_input)
-            print(f'2: {len(cities_set)}')
-            for city in cities_set:
-                if city[0] == user_input[-1].upper():
-                    cities_set.remove(city)
-                    print(f'3: {len(cities_set)}')
-                    user_input = input(f'Последняя буква: {user_input[-1].upper()}: {city}, \n'
-                                       f'Ваш ход, введите город на {city[-1].upper()}:  ')
-                    # if user_input[0] == city[-1].upper():
-                    #     break
-                    # else:
-                    #     print(f'Не с той буквы, нужна буква {city[-1].upper()}')
-                    #     break
-        # elif user_input not in cities_set:
-        #     user_input = input(f'Город {user_input} не подходит! Введите другой: ')
-        else:
-            user_input = input(f'Город {user_input} не подходит, еще попытка:  ')
-            # print(f'Город {user_input} не подходит (else)!')
-            break
+for city in list(cities_set):
 
-        #         next_move = [city for city in cities_set if city[0] == last_char]
-        # user_input = input(f'Последняя буква {last_char}, Город: {next_city} ')
-        # for city in cities_set:
-        # if cities_set[0] == last_char:
-        #     user_input = input(f'Мой ход {city}')
-        # next_move = [city for city in cities_set if city[0] == last_char]
-
-    else:
-        print('Ха-ха-ха! Ты проиграл, человек!')
+    if user_input in cities_set:
+        cities_set.remove(user_input)
+        print(f'Мой вариант на {user_input[-1].capitalize()}')
+    elif user_input[-1] == 'ь' or user_input[-1] == 'ы' or user_input[-1] == 'й':
+        user_input = input('Введите другое название: ').capitalize()
+    if user_input == 'Стоп':
+        print('Мы еще не начали, а вы уже сдались!')
         break
 
-
-
-
+    # выбираем название для хода компьютера
+    if city[0] == user_input[-1].upper() and city[-1] != 'ь' and city[-1] != 'ы' and city[-1] != 'й':
+        cities_set.remove(city)
+        user_input = input(f'Город {city}, ваш вариант на {city[-1].upper()}: ').capitalize()
+        # проверяем следующий ход пользователя
+        if user_input in cities_set and user_input[0] == city[-1].upper():
+            cities_set.remove(user_input)
+            print(f'Хороший ход, теперь мой вариант на {user_input[-1].capitalize()}')
+        elif user_input == 'Стоп':
+            print('Вы сдались!')
+            break
+        else:
+            print(f'{user_input} не подходит! Вы проиграли!')
+            break
