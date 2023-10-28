088fc04330737e5026817c54c96241f4327c02b7
PJ <69799563+pj-web@users.noreply.github.com>
2023-10-29 02:50:56 +0400

    upd

--- first_project/les20.py
+++ first_project/les20.py
@@ -6,7 +6,11 @@ URL = 'http://www.google.com'
 
 options = webdriver.ChromeOptions()
 
-options.add_argument("--window-size=1920,1080")
+# options.add_argument("--window-size=1920,1080")  # window size
+options.add_argument("--start-maximized")  # развернуть на весь экран
+# options.add_argument("--disable-gpu")  # disable GPU
+# options.add_argument("--headless=new")  # отключение отображения браузера
+
 
 driver = webdriver.Chrome(options=options)
 driver.get(URL)
