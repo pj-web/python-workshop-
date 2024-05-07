78d6c5e2abeff0151d0051d6575b3cc3cbaee250
PJ <69799563+pj-web@users.noreply.github.com>
2024-05-07 22:00:11 +0400

    +les21+hw8-begin

--- first_project/hw7/hw7.py
+++ first_project/hw7/hw7.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 
-def choose_browser():
+def choose_browser() -> webdriver:
     browser_type_input = input('Для какого браузера создать веб-драйвер?\n'
                                'Google Chrome - введите 1\n'
                                'Firefox - введите 2\n'
@@ -9,11 +9,11 @@ def choose_browser():
                                'Введите сюда: ')
     # Выбор браузера в зависимости от введенного пользователем значения:
     if browser_type_input == '1':
-        driver = webdriver.Chrome()
+        driver: webdriver.Chrome = webdriver.Chrome()
     elif browser_type_input == '2':
-        driver = webdriver.Firefox()
+        driver: webdriver.Firefox = webdriver.Firefox()
     elif browser_type_input == '3':
-        driver = webdriver.Edge()
+        driver: webdriver.Edge = webdriver.Edge()
     else:
         raise ValueError(f"Ошибка: некорректное значение: {browser_type_input}")
 
