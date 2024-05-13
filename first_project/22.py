250ef4aa3f1835fcf53192a637af8e6fa99e5ffb
PJ <69799563+pj-web@users.noreply.github.com>
2024-05-13 22:03:25 +0400

    hw9

--- first_project/22.py
+++ first_project/22.py
@@ -63,66 +63,66 @@ def main():
         })
 
     # Пишем все в JSON с отступами и ensure_ascii=False и json dump
-    with open('result.json', 'w', encoding='utf-8') as file:
-        json.dump(result, file, indent=4, ensure_ascii=False)
-
-    # Читаем JSON и сохраняем его в CSV с кодировкой виндовс и разделителем и ньюлайн
-    with open('result.json', 'r', encoding='utf-8') as file:
-        result = json.load(file)
-
-    with open('result.csv', 'w', encoding='utf-8', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        writer.writerow(['title', 'url', 'img_url', 'price'])
-        for item in result:
-            writer.writerow([item['title'], item['url'], item['img_url'], item['price']])
+    # with open('result.json', 'w', encoding='utf-8') as file:
+    #     json.dump(result, file, indent=4, ensure_ascii=False)
+    #
+    # # Читаем JSON и сохраняем его в CSV с кодировкой виндовс и разделителем и ньюлайн
+    # with open('result.json', 'r', encoding='utf-8') as file:
+    #     result = json.load(file)
+    #
+    # with open('result.csv', 'w', encoding='utf-8', newline='') as file:
+    #     writer = csv.writer(file, delimiter=';')
+    #     writer.writerow(['title', 'url', 'img_url', 'price'])
+    #     for item in result:
+    #         writer.writerow([item['title'], item['url'], item['img_url'], item['price']])
 
     pprint(result)
 
 
 # Работа с таблицами
 
-def main():
-    # Создаем драйвер
-    driver: webdriver.Chrome = get_driver()
-    # Переходим на страницу
-    get_page(LOCAL_PAGE, driver)
-    # Получаем тело таблицы
-    table_body: WebElement = driver.find_element(By.TAG_NAME, 'tbody')
-
-    # Получаем заголовки столбцов из thead
-    header: WebElement = driver.find_element(By.TAG_NAME, 'thead')
-    # Получаем все ячейки заголовка
-    header_cells: List[WebElement] = header.find_elements(By.TAG_NAME, 'th')
-
-    # Получаем все строки таблицы
-    rows: List[WebElement] = table_body.find_elements(By.TAG_NAME, 'tr')
-
-    # Создаем пустой список для результатов
-    result: List[List[str]] = []
-
-    # Добавляем в результат список заголовков через list comprehension
-    result.append([cell.text for cell in header_cells])
-
-    # Проходимся по всем строкам
-    for row in rows:
-        # Получаем все ячейки в строке
-        cells: List[WebElement] = row.find_elements(By.TAG_NAME, 'td')
-        # Создаем пустой список для ячеек
-        row_result: List[str] = []
-        # Проходимся по всем ячейкам
-        for cell in cells:
-            # Добавляем текст ячейки в список
-            row_result.append(cell.text)
-        # Добавляем список ячеек в результат
-        result.append(row_result)
-
-    print(result)
-
-    # Пишем в CSV с разделителем и ньюлайн и кодировкой виндовс
-    with open('result_table.csv', 'w', encoding='windows-1251', newline='') as file:
-        writer = csv.writer(file, delimiter=';')
-        for row in result:
-            writer.writerow(row)
+# def main():
+#     # Создаем драйвер
+#     driver: webdriver.Chrome = get_driver()
+#     # Переходим на страницу
+#     get_page(LOCAL_PAGE, driver)
+#     # Получаем тело таблицы
+#     table_body: WebElement = driver.find_element(By.TAG_NAME, 'tbody')
+#
+#     # Получаем заголовки столбцов из thead
+#     header: WebElement = driver.find_element(By.TAG_NAME, 'thead')
+#     # Получаем все ячейки заголовка
+#     header_cells: List[WebElement] = header.find_elements(By.TAG_NAME, 'th')
+#
+#     # Получаем все строки таблицы
+#     rows: List[WebElement] = table_body.find_elements(By.TAG_NAME, 'tr')
+#
+#     # Создаем пустой список для результатов
+#     result: List[List[str]] = []
+#
+#     # Добавляем в результат список заголовков через list comprehension
+#     result.append([cell.text for cell in header_cells])
+#
+#     # Проходимся по всем строкам
+#     for row in rows:
+#         # Получаем все ячейки в строке
+#         cells: List[WebElement] = row.find_elements(By.TAG_NAME, 'td')
+#         # Создаем пустой список для ячеек
+#         row_result: List[str] = []
+#         # Проходимся по всем ячейкам
+#         for cell in cells:
+#             # Добавляем текст ячейки в список
+#             row_result.append(cell.text)
+#         # Добавляем список ячеек в результат
+#         result.append(row_result)
+#
+#     print(result)
+#
+#     # Пишем в CSV с разделителем и ньюлайн и кодировкой виндовс
+#     with open('result_table.csv', 'w', encoding='windows-1251', newline='') as file:
+#         writer = csv.writer(file, delimiter=';')
+#         for row in result:
+#             writer.writerow(row)
 
 
 main()
--- first_project/hw/hw9.py
+++ first_project/hw/hw9.py
@@ -1,7 +1,8 @@
 from selenium import webdriver  # Импорт webdriver
 from selenium.webdriver.common.by import By  # Инструмент By.
 from selenium.webdriver.remote.webelement import WebElement
-from typing import List
+from typing import List, Dict
+from pprint import pprint
 
 WEB_DRIVER_OPTIONS = ['--disable-gpu', '--headless=new']
 URL = 'http://books.toscrape.com/'
@@ -19,23 +20,78 @@ def get_driver(driver_options: list) -> webdriver.Chrome:
 
 
 # Переход на страницу
-def get_page(url: str, driver: webdriver.Chrome) -> None:
-    driver.get(url)
+def get_page(url: str, web_drv: webdriver.Chrome) -> None:
+    web_drv.get(url)
 
 
-# Находим все элементы с классом заданным в аргументе, по умолчанию 'product_pod'
-def get_all_class_names(attribute_value: str = 'product_pod'):
-    driver: webdriver.Chrome = get_driver(WEB_DRIVER_OPTIONS)
-    get_page(URL, driver)
-    all_class_names: List[WebElement] = driver.find_elements(By.CLASS_NAME,  attribute_value)
-    return all_class_names
+driver: webdriver.Chrome = get_driver(WEB_DRIVER_OPTIONS)
+
+
+# 1. Функция для нахождения всех карточек на одной странице.
+def get_all_cards(attribute_value: str = 'product_pod'):
+    all_cards: List[WebElement] = driver.find_elements(By.CLASS_NAME,  attribute_value)
+    return all_cards
+
+
+# 2. Функция для извлечения данных из одной карточки.
+def get_data_from_card(input_cards: List[WebElement]):
+    result: List[Dict[str, str]] = []
+
+    # Получаем данные из первого элемента с индексом 0
+    title: str = input_cards[0].find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('title')
+    url: str = input_cards[0].find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('href')
+    img_url: str = input_cards[0].find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+    price: str = input_cards[0].find_element(By.CSS_SELECTOR, '.price_color').text
+
+    result.append({
+        'title': rf"{title}",
+        'url': rf"{url}",
+        'img_url': rf"{img_url}",
+        'price': rf"{price}"
+    })
+
+    pprint(result)
+
+
+# 3. Функция для извлечения данных со всех карточек на одной странице.
+def get_data_from_cards(input_cards: List[WebElement]):
+    result: List[Dict[str, str]] = []
+
+    for card in input_cards:
+        title: str = card.find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('title')
+        url: str = card.find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('href')
+        img_url: str = card.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
+        price: str = card.find_element(By.CSS_SELECTOR, '.price_color').text
+
+        result.append({
+            'title': rf"{title}",
+            'url': rf"{url}",
+            'img_url': rf"{img_url}",
+            'price': rf"{price}"
+        })
+
+    pprint(result)
+
+
+# 4. Функция для определения количества страниц на сайте.
+def get_page_count(driver: webdriver.Chrome) -> None:
+
+    # Находим элемент li.current - последний элемент с конца - это номер последней страницы
+    current_element: WebElement = driver.find_element(By.CSS_SELECTOR, 'li.current')
+    last_page_number: int = int(current_element.text.split(' ')[-1])
+    print(f'Количество страниц: {last_page_number}')
+
 
 
 def main():
 
-    # Находим все продуктовые карточки на странице по имени класса
-    # product_pods: List[WebElement] = driver.find_elements(By.CLASS_NAME, 'product_pod')
-    print(get_all_class_names())
+    get_page(URL, driver)
+
+    get_data_from_card(get_all_cards())
+
+    get_data_from_cards(get_all_cards())
+
+    get_page_count(driver)
 
 
 main()
