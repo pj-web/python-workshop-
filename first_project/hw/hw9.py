c41aac933dd81b6f1b0421358ade13af91da9215
PJ <69799563+pj-web@users.noreply.github.com>
2024-05-15 02:41:08 +0400

    hw9

--- first_project/hw/hw9.py
+++ first_project/hw/hw9.py
@@ -32,44 +32,14 @@ driver: webdriver.Chrome = get_driver(WEB_DRIVER_OPTIONS)
 
 # 1. Функция для нахождения всех карточек на одной странице.
 # Аргумент функции - имя CSS класса, по умолчанию 'product_pod'
-def get_all_cards(attribute_value: str = 'product_pod'):
-    all_cards: List[WebElement] = driver.find_elements(By.CLASS_NAME,  attribute_value)
-    return all_cards
+def get_all_cards(driver: webdriver.Chrome):
+    return driver.find_elements(By.CLASS_NAME,  'product_pod')
 
 
 # 2. Функция для извлечения данных из одной карточки, номер карточки вводит пользователь.
 def get_data_from_card(input_card: WebElement):
-    # index: int = int(input(f'Введите номер карточки (всего их {len(get_all_cards())}): ')) - 1
-    # result: List[Dict[str, str]] = []
-    #
-    # # Проверяем, чтобы введенное значение было не больше, чем число карточек на странице
-    # if index <= len(get_all_cards()):
-    #     # Получаем данные из элемента переданного в аргументе index
-    #     title: str = input_cards[index].find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('title')
-    #     url: str = input_cards[index].find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('href')
-    #     img_url: str = input_cards[index].find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
-    #     price: str = input_cards[index].find_element(By.CSS_SELECTOR, '.price_color').text
-    #
-    #     # Рейтинг лежит в p.star-rating (Классы One Two Three Four Five)
-    #     rating_element: WebElement = input_cards[index].find_element(By.CSS_SELECTOR, 'p.star-rating')
-    #     # Достаем классы. Последний - это рейтинг
-    #     rating: str = rating_element.get_attribute('class').split(' ')[-1]
-    #
-    #     result.append({
-    #         'title': rf"{title}",
-    #         'url': rf"{url}",
-    #         'img_url': rf"{img_url}",
-    #         'price': rf"{price}",
-    #         'rating': rf"{rating}"
-    #     })
-    #
-    #     # pprint(result)
-    #     return result
-    # else:
-    #     print(f'Нужно значение значение от 0 до {len(get_all_cards())}')
 
-
-    # Получаем данные из элемента переданного в аргументе index
+    # Получаем данные из элемента переданного в аргументе
     title: str = input_card.find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('title')
     url: str = input_card.find_element(By.CSS_SELECTOR, 'h3 a').get_attribute('href')
     img_url: str = input_card.find_element(By.CSS_SELECTOR, 'img').get_attribute('src')
@@ -87,9 +57,6 @@ def get_data_from_card(input_card: WebElement):
         'price': rf"{price}",
         'rating': rf"{rating}"
     }
-
-    # pprint(result)
-    # return result
 
 
 # 4. Функция для определения количества страниц на сайте.
@@ -149,14 +116,16 @@ def get_all_pages():
         get_page(page_url, driver)
         # Проверяем статус код
         # Получаем все карточки товаров на странице
-        product_pods: List[WebElement] = get_all_cards(driver)
+        product_pods: list[WebElement] = get_all_cards(driver)
         # Перебираем все карточки товаров
         for product_pod in product_pods:
             # Получаем данные по карточке товара
             product_pod_data: Dict[str, str] = get_data_from_card(product_pod)
             # Добавляем данные в список
             data.append(product_pod_data)
+            print(f'Содержимое страницы № {page_number}:')
             pprint(data)
+            print('----------------------------------------------')
 
     # Закрываем драйвер
     driver.close()
@@ -176,4 +145,5 @@ def get_all_pages():
 # main()
 
 get_all_pages()
+
 
