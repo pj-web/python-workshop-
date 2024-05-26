9653dc552c5306e69ef79cdbf88e19caf3a5571d
PJ <69799563+pj-web@users.noreply.github.com>
2024-05-26 12:06:34 +0400

    Update hw10.py

--- first_project/hw/hw10.py
+++ first_project/hw/hw10.py
@@ -42,23 +42,22 @@ driver: webdriver.Chrome = get_driver(WEB_DRIVER_OPTIONS)
 
 
 get_page(URL, driver)
-sleep(15)
+# Нажимаем кнопку "Вход"
 print(driver.find_element(By.XPATH, '//*[@id="navigation"]/div/nav/div/ul[2]/li[1]/label/a').text)
 driver.find_element(By.XPATH, '//*[@id="navigation"]/div/nav/div/ul[2]/li[1]/label/a').click()
-# driver.find_element(By.CSS_SELECTOR, '#navigation > div > nav > div > ul.visitorTabs > li:nth-child(1) > label > a').click()
-sleep(5)
+# После каждого шага джем 3 секунды
+sleep(3)
 
-input_login = driver.find_element(By.XPATH, '//*[@id="LoginControl"]')
-input_login.send_keys("dr.gonzo.mail71@gmail.com")
-sleep(5)
+input_login = driver.find_element(By.XPATH, '//*[@id="LoginControl"]').send_keys("dr.gonzo.mail71@gmail.com")
+sleep(3)
 # //*[@id="LoginControl"]
 # /html/body/div[1]/div/div/div/div/form/div/dl[1]/dd/input
 
-input_password = driver.find_element(By.XPATH, '//*[@id="ctrl_password"]')
-input_password.send_keys("!sc!PJ!41779!")
+driver.find_element(By.XPATH, '//*[@id="ctrl_password"]').send_keys("!sc!PJ!41779!")
 # //*[@id="ctrl_password"]
 # /html/body/div[1]/div/div/div/div/form/div/dl[2]/dd/ul/li[3]/input
 
+driver.find_element(By.XPATH, '//*[@id="login"]/div/dl[3]/dd/input').click()
 
 sleep(5)
 code_2fa = input('Введите код двухфакторной аутентификации: ')
@@ -66,6 +65,8 @@ sleep(5)
 input_code_2fa = driver.find_element(By.XPATH, '//*[@id="ctrl_totp_code"]')
 input_code_2fa.send_keys(code_2fa)
 sleep(5)
+driver.find_element(By.XPATH, '//*[@id="content"]/div/div/form/dl[3]/dd/ul/li/label/input').click()
+driver.find_element(By.XPATH, '//*[@id="content"]/div/div/form/dl[4]/dd/input').click()
 
 # //*[@id="ctrl_totp_code"]
 # /html/body/div[1]/div[2]/div/div/form/dl[2]/dd/input
